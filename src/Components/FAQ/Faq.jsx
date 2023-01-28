0954ef94d07e9ff0917ef569f920384378a28723
Tanmay-Patel-21 <tanmayapatel21@gmail.com>
2023-01-28 10:08:37 +0530

    frontend changes

--- src/Components/FAQ/Faq.jsx
+++ src/Components/FAQ/Faq.jsx
@@ -3,35 +3,53 @@ import Navbar from "../Navbar/Navbar"
 function Faq() {
     return (
         <div id="bg" >
-            <Navbar />
-            
-            <div id="header">
-
-            </div>
             <div id="container">
-                <br /><br />
                 <h1>Frequently Asked Questions</h1>
-                <div id="Questions">
-                    <h2>What is the carreer guidance tool?</h2>
-                    <h2>It's an AI based tool that helps you predict the best carreer choice for you according to your specific skills and interests.</h2><br />
-                    <h2>How accurate is the carreer guidance tool?</h2>
-                    <h2></h2><br />
-                    <h2>Can I trust the predictions of this tool?</h2>
-                    <h2>You know yourself and your needs best, this tool is just a guiding hand to help you rediscover that.</h2><br />
-                    <h2>How does it work?</h2>
-                    <h2></h2><br />
-                    <h2>Do I need to sign up to use the carreer guidance tool?</h2>
-                    <h2>No, there is no need to sign up or log in, you can use it directly.</h2><br />
-                    <h2>How does this help anyone?</h2>
-                    <h2>This tool not only predicts the right choice for you but also gives you a rundown of all the skills you need to make it happen!</h2><br />
-                    <h2>I know what I want to do and I have the skills for it, how do I apply to jobs?</h2>
-                    <h2>You will need a good resume. Head over to the <a href="">Resume Builder</a> to create one in minutes, no editing skills required.</h2>
+                <div id="faqContainer">
+                    <div className="question">
+
+                        What is the carreer guidance tool?
+                    </div>
+                    <div className="answer">
+
+                        It's an AI based tool that helps you predict the best carreer choice for you according to your specific skills and interests.
+                    </div>
+                    {/* <div className="question">
+                        How accurate is the carreer guidance tool?
+                    </div> */}
+                    <div className="question">
+                        Can I trust the predictions of this tool?
+                    </div>
+                    <div className="answer">
+                        You know yourself and your needs best, this tool is just a guiding hand to help you rediscover that.
+                    </div>
+                    {/* <div className="question">
+                        How does it work?
+                    </div> */}
+                    <div className="question">
+                        Do I need to sign up to use the carreer guidance tool?
+                    </div>
+                    <div className="answer">
+                        No, there is no need to sign up or log in, you can use it directly.
+                    </div>
+                    <div className="question">
+                        How does this help anyone?
+                    </div>
+                    <div className="answer">
+                        This tool not only predicts the right choice for you but also gives you a rundown of all the skills you need to make it happen!
+                    </div>
+                    <div className="question">
+                        I know what I want to do and I have the skills for it, how do I apply to jobs?
+                    </div>
+                    <div className="answer">
+                        You will need a good resume. Head over to the Resume Builder to create one in minutes, no editing skills required.                    </div>
+
                 </div>
 
 
-            </div>
+            </div >
 
-        </div>
+        </div >
 
     )
 }
