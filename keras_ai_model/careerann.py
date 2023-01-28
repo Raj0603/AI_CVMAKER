a06b4a1408e1abae22eb7e2f746503febf9d32c9
Rishabh Nahar <rishabhnahar1202@gmail.com>
2023-01-28 05:59:56 +0530

    Backend

--- keras_ai_model/careerann.py
+++ keras_ai_model/careerann.py
@@ -35,6 +35,8 @@ X[:,:] = imputer.transform(X[:,:])
 y = dataset.iloc[:, -1].values
 
 
+y_roles = y
+
 
 # Encoding categorical data
 # Label Encoding column
@@ -194,14 +196,13 @@ y_pred = (y_pred >0.5)
 from sklearn.metrics import multilabel_confusion_matrix
 cm = multilabel_confusion_matrix(y_test,y_pred)
 
-in_dat = np.asarray([sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],sys.argv[9],sys.argv[10],sys.argv[11],
-                sys.argv[12],sys.argv[13],sys.argv[14],sys.argv[15],sys.argv[16],sys.argv[17],sys.argv[18],sys.argv[19],sys.argv[20]])
+in_dat = tf.constant([1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1])
 tf.constant(in_dat)
 print(in_dat)
 
 single_pred = ann.predict(sc.transform([in_dat]))
 
-single_pred = (single_pred > 0.2)
+single_pred = (single_pred > 0.1)
 
 print(single_pred)
 
